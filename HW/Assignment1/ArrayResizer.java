c58b70180989f94ba2cf0cae1c3468ea660bb623
Jiuru Lyu <lvjiuru@hotmail.com>
2023-08-29 18:23:35 -0400

    assignment1 complete

--- HW/Assignment1/ArrayResizer.java
+++ HW/Assignment1/ArrayResizer.java
@@ -9,14 +9,46 @@ public class ArrayResizer {
 
     public static boolean isNonZeroRow(int[][] array2D, int r) {
         // Write this method for part A
-        return false;  // Statement added to prevent compile error
-		               // Remove it when you write this method
+        for (int i = 0; i < array2D.length; i++) {
+            if (i == r) {
+                for (int j = 0; j < array2D[i].length; j++) {
+                    if (array2D[i][j] == 0) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
     }
 
     public static int[][] resize(int[][] array2D) {
         // Write this method for part B
-        return null; // Statement added to prevent compile error
-                    // Remove it when you write this method
+        int numRow = array2D.length;
+        for (int i = 0; i < array2D.length; i++) {
+            for (int j = 0; j < array2D[i].length; j++) {
+                if (array2D[i][j] == 0) {
+                    numRow--;
+                    break;
+                }
+            }
+        }
+        int[][] result = new int[numRow][array2D[0].length];
+        int index = 0;
+        for (int i = 0; i < array2D.length; i++) {
+            int count = 0;
+            for (int j = 0; j < array2D[i].length; j++) {
+                if (array2D[i][j] != 0) {
+                    count++;
+                }
+            }
+            if (count == array2D[i].length) {
+                for (int j = 0; j < array2D[i].length; j++) {
+                    result[index][j] = array2D[i][j];
+                }
+                index++;
+            }
+        }
+        return result;
     }
 
     public static int numNonZeroRows(int[][] array2D) {
