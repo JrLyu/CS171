bf94356194a8cc3578b290ce428288c3d6ee2fa0
Jiuru Lyu <lvjiuru@hotmail.com>
2023-10-12 16:30:37 -0400

    hw6 update

--- HW/Assignment6/InfixToPostfix.java
+++ HW/Assignment6/InfixToPostfix.java
@@ -3,6 +3,37 @@ import java.util.ArrayList;
 
 public class InfixToPostfix {
     public static ArrayList<String> convertToPostfix(String[] inp) {
-        return null;
+        ArrayList<String> result = new ArrayList<>();
+        Stack<String> operatorStck = new Stack<>();
+        for (int i = 0; i < inp.length; i++) {
+            String s = inp[i];
+            if (s.equals("+") || s.equals("-")) { // If s is an operator
+                if (!operatorStck.isEmpty()) {
+                    if (operatorStck.peek().equals("+") || operatorStck.peek().equals("-")) {
+                        result.add(operatorStck.pop());
+                    }
+                }
+                operatorStck.push(s);
+            } else if (s.equals("(")) {
+                operatorStck.push(s);
+            } else if (s.equals(")")) {
+                while (!operatorStck.peek().equals("(")) {
+                    result.add(operatorStck.pop());
+                }
+                operatorStck.pop();
+            } else {
+                // If s is a number
+                result.add(s);
+            }
+        }
+        // When the input is exhusted, pop out all the operators reamined in the stack
+        while (!operatorStck.isEmpty()) {
+            if (!operatorStck.peek().equals("(")) {
+                result.add(operatorStck.pop());
+            } else {
+                operatorStck.pop();
+            }
+        }
+        return result;
     }
 }
