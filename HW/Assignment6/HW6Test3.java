6994d8bf63e410d0773e4815fc5c86296ec6796c
Jiuru Lyu <lvjiuru@hotmail.com>
2023-10-17 14:14:41 -0400

    oct17 lec

---
+++ HW/Assignment6/HW6Test3.java
@@ -0,0 +1,33 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class HW6Test3 {
+    public static void main(String[] args) {
+        ArrayList<String> s;
+        String[][] input = {
+                            { "(", "3", "+", "4" ,")"},
+                            { "(", "3", "-", "4" ,")"},
+                           };
+
+        String[] ans = { "[3, 4, +]",
+                         "[3, 4, -]"  };
+
+        int err = 0;
+
+        for (int i = 0; i < input.length; i++) {
+            System.out.println("Input   = " + Arrays.toString(input[i]));
+            s = InfixToPostfix.convertToPostfix(input[i]);
+            System.out.println("Output  = " + s + "\n");
+
+            if (!s.toString().equals(ans[i])) {
+                err++;
+                System.out.println("Wrong, correct answer = " + ans[i]);
+            }
+        }
+
+        if (err == 0) {
+            System.out.println("Test 3 passed");
+        }
+    }
+}
